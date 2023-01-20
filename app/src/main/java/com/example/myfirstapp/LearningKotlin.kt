28087ec50ac11014aac8ba0f46062caaa1386f66
Julian Gutierrez <julian.milton.gutierrez@gmail.com>
2023-01-19 22:40:41 -0600

    learned how to create variables

--- app/src/main/java/com/example/myfirstapp/LearningKotlin.kt
+++ app/src/main/java/com/example/myfirstapp/LearningKotlin.kt
@@ -2,5 +2,10 @@ package com.example.myfirstapp
 
 fun main(args:Array<String>)
 {
-    println("Hello World!")
+//    println("Hello World!")
+
+    // to create a variable it is similar to how you create a variable in javascript, the only difference is that if you can assign the variable type after the variable name with : (Int, String, Boolean, Float)
+    var age = 29
+
+    var candy:String = "Fun Dip"
 }
